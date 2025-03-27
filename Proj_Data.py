e020f01c2b991a2baf09e5432fae161ddd9215ce
Abhiram Thotkura <athotkura@gmail.com>
2025-03-26 20:45:09 -0400

    Added Copy Functionality

--- Proj_Data.py
+++ Proj_Data.py
@@ -23,15 +23,33 @@ def create_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
+def change_names_from_paths(paths, to_add):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_add, "Compiled_Project")
+        new_names.append(new_dir_name)
 
+    return new_names
+
+def copy_and_overwrite(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    shutil.copytree(source,destination)
 
 def main(source, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source) #Adds path to the source directory relative to the current working directory
     target_path = os.path.join(cwd, target)
-
+    
+    create_dir(target_path)
+    
     proj_paths = find_all_proj_paths(source_path)
-    print(proj_paths)
+    new_proj_paths = change_names_from_paths(proj_paths, "Project")
+
+    for src, dest in zip(proj_paths, new_proj_paths):
+        dest_path = os.path.join(target_path, dest)
+        copy_and_overwrite(src, dest_path)
 
 
 if __name__ == "__main__":
---
+++ target/Compiled_Project_4/project4.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello Project 4" << std::endl; 
+}
